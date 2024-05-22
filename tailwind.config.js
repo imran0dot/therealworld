20a64b986533bf9165da9263de1ebd31a4088f76
imran0dot <imran0dot@gmail.com>
2024-05-22 18:21:16 +0600

    tailwind setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-background":  "#060e15",
+        "secondary-white": "rgba(255, 255, 255, .5)",
+        "yellow": "#ffcf23",
+        "brand": "#ffbb38",
+        "orange": "#ff8d3a",
+        "gold": "#d0b167",
+        "light-gold": "#d0b167",
+        "secondary-light-gold": "#ddc079",
+        "secondary-dark-gold": "#ad8b3a"
+      }
+    },
   },
   plugins: [],
 }
