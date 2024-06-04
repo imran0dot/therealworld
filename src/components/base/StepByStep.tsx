20b70017d0dd5496565f8283f2e02ef2af397a4d
imran0dot <imran0dot@gmail.com>
2024-06-04 19:47:13 +0600

    little [update]

--- src/components/base/StepByStep.tsx
+++ src/components/base/StepByStep.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import Container from '../shared/Container';
 import MainTitle from '../shared/MainTitle';
 import ImageOne from '../../assets/img/step-path-01.webp'
@@ -9,6 +9,11 @@ import FeatureList from '../ui/FeatureList';
 import arrowSVG from '../../assets/svg-images/svgexport-23.svg'
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import GiantButton from '../ui/GiantButton';
+import { FaGraduationCap, FaUsers } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa6";
+import { useGSAP } from '@gsap/react';
+
 
 
 
@@ -17,7 +22,29 @@ const StepByStep: React.FC = () => {
     const parentRef = useRef(null);
     gsap.registerPlugin(ScrollTrigger);
 
-    useEffect(() => {
+    // useEffect(() => {
+    //     const arrow = arrowRef.current;
+    //     const parent = parentRef.current as HTMLDivElement | null;
+
+    //     if (arrow && parent) {
+    //         gsap.to(arrow, {
+    //             y: parent.clientHeight * 1.2,
+    //             // translateY: parent.clientHeight,
+    //             ease: "power1.inOut",
+    //             duration: 5,
+    //             scrollTrigger: {
+    //                 trigger: arrow,
+    //                 start: "top bottom",
+    //                 end: "bottom top",
+    //                 scrub: 0.05,
+    //                 markers: true
+    //             }
+    //         });
+    //     }
+    // }, []);
+
+
+    useGSAP(() => {
         const arrow = arrowRef.current;
         const parent = parentRef.current as HTMLDivElement | null;
 
@@ -36,8 +63,7 @@ const StepByStep: React.FC = () => {
                 }
             });
         }
-    }, []);
-
+    });
 
     return (
         <div>
@@ -49,93 +75,102 @@ const StepByStep: React.FC = () => {
                 </div>
 
                 <div
-                    ref={parentRef}
                     className='bg-center flex flex-col gap-20 md:items-center relative w-full mt-20 min-h-screen overflow-hidden'
                     style={{ backgroundImage: `url('https://www.jointherealworld.com/revamp/images/cubes-bg.svg')` }}>
 
 
-                    {/* box one  */}
-                    <div>
-                        <Container>
-                            <div className='grid grid-cols-1 md:grid-cols-2 px-10'>
-                                <div className='relative flex justify-center items-center'>
-                                    <img className='z-20' width={'80%'} src={ImageOne} alt="" />
-                                    <div className='w-20 h-20 left-20 top-20 opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
-                                    <div className='w-20 h-20 trans opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
+                    <div
+                        ref={parentRef}>
+                        {/* box one  */}
+                        <div>
+                            <Container>
+                                <div className='grid grid-cols-1 md:grid-cols-2 px-10'>
+                                    <div className='relative flex justify-center items-center'>
+                                        <img className='z-20' width={'80%'} src={ImageOne} alt="" />
+                                        <div className='w-20 h-20 left-20 top-20 opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
+                                        <div className='w-20 h-20 trans opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
+                                    </div>
+                                    <div className='md:px-10'>
+                                        <FeatureList
+                                            icon={FaGraduationCap}
+                                            title='VITAL LIFE LESSONS'
+                                            features={
+                                                [
+                                                    'World-class custom built learning applicatio',
+                                                    'Scale from <strong class=text-white>Zero to $10k/month</strong> as fast as possible',
+                                                    'Master skills you need to <strong class=text-white>maximize your income/month</strong>'
+                                                ]}
+                                        />
+                                    </div>
                                 </div>
-                                <div className='md:px-10'>
-                                    <FeatureList
-                                        title='VITAL LIFE LESSONS'
-                                        features={
-                                            [
-                                                'World-class custom built learning applicatio',
-                                                'Scale from <strong class=text-white>Zero to $10k/month</strong> as fast as possible',
-                                                'Master skills you need to <strong class=text-white>maximize your income/month</strong>'
-                                            ]}
-                                    />
+                            </Container>
+                        </div>
+
+
+                        {/* box two  */}
+                        <div>
+                            <Container>
+                                <div className='grid grid-cols-1 md:grid-cols-2 px-10'>
+                                    <div className='relative flex justify-center items-center'>
+                                        <img className='z-20' width={'80%'} src={ImageTwo} alt="" />
+                                        <div className='w-20 h-20 left-20 top-20 opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
+                                        <div className='w-20 h-20 trans opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
+                                    </div>
+                                    <div className='md:px-10'>
+                                        <FeatureList
+                                            icon={FaUsers}
+                                            title='VITAL LIFE LESSONS'
+                                            features={
+                                                [
+                                                    'World-class custom built learning applicatio',
+                                                    'Scale from <strong class=text-white>Zero to $10k/month</strong> as fast as possible',
+                                                    'Master skills you need to <strong class=text-white>maximize your income/month</strong>'
+                                                ]}
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                        </Container>
+                            </Container>
+                        </div>
+
+                        {/* box three  */}
+                        <div>
+                            <Container>
+                                <div className='grid grid-cols-1 md:grid-cols-2 px-10'>
+                                    <div className='relative flex justify-center items-center'>
+                                        <img className='z-20' width={'80%'} src={ImageThree} alt="" />
+                                        <div className='w-20 h-20 left-20 top-20 opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
+                                        <div className='w-20 h-20 trans opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
+                                    </div>
+                                    <div className='md:px-10'>
+                                        <FeatureList
+                                            icon={FaUserTie}
+                                            title='VITAL LIFE LESSONS'
+                                            features={
+                                                [
+                                                    'World-class custom built learning applicatio',
+                                                    'Scale from <strong class=text-white>Zero to $10k/month</strong> as fast as possible',
+                                                    'Master skills you need to <strong class=text-white>maximize your income/month</strong>'
+                                                ]}
+                                        />
+                                    </div>
+                                </div>
+                            </Container>
+                        </div>
                     </div>
 
 
-                    {/* box two  */}
-                    <div>
-                        <Container>
-                            <div className='grid grid-cols-1 md:grid-cols-2 px-10'>
-                                <div className='relative flex justify-center items-center'>
-                                    <img className='z-20' width={'80%'} src={ImageTwo} alt="" />
-                                    <div className='w-20 h-20 left-20 top-20 opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
-                                    <div className='w-20 h-20 trans opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
-                                </div>
-                                <div className='md:px-10'>
-                                    <FeatureList
-                                        title='VITAL LIFE LESSONS'
-                                        features={
-                                            [
-                                                'World-class custom built learning applicatio',
-                                                'Scale from <strong class=text-white>Zero to $10k/month</strong> as fast as possible',
-                                                'Master skills you need to <strong class=text-white>maximize your income/month</strong>'
-                                            ]}
-                                    />
-                                </div>
-                            </div>
-                        </Container>
-                    </div>
+                    <div className='absolute h-full w-[3px] bg-gradient-to-b from-transparent to-white ml-2 md:ml-0'></div>
 
-                    {/* box three  */}
-                    <div>
-                        <Container>
-                            <div className='grid grid-cols-1 md:grid-cols-2 px-10'>
-                                <div className='relative flex justify-center items-center'>
-                                    <img className='z-20' width={'80%'} src={ImageThree} alt="" />
-                                    <div className='w-20 h-20 left-20 top-20 opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
-                                    <div className='w-20 h-20 trans opacity-30 bg-yellow absolute z-10 blur-3xl'></div>
-                                </div>
-                                <div className='md:px-10'>
-                                    <FeatureList
-                                        title='VITAL LIFE LESSONS'
-                                        features={
-                                            [
-                                                'World-class custom built learning applicatio',
-                                                'Scale from <strong class=text-white>Zero to $10k/month</strong> as fast as possible',
-                                                'Master skills you need to <strong class=text-white>maximize your income/month</strong>'
-                                            ]}
-                                    />
-                                </div>
-                            </div>
-                        </Container>
-                    </div>
-
-
-                    <div className='absolute h-full w-[5px] bg-gradient-to-b from-transparent to-white ml-2 md:ml-0'></div>
-
-                    <div 
-                     ref={arrowRef}
-                      className='absolute flex flex-col justify-start items-center top-0 bottom-0'>
+                    <div
+                        ref={arrowRef}
+                        className='absolute flex flex-col justify-start items-center top-0 bottom-0'>
                         <img className='top-0 object-scale-down z-20 ' src={arrowSVG} alt="" />
-                        <div className='absolute h-full w-[5px] bg-secondary-dark '></div>
+                        <div className='absolute h-full w-[3px] bg-secondary-dark '></div>
                     </div>
+
+
+                    {/* button  */}
+                    <GiantButton smallText>Join The Real World</GiantButton>
                 </div>
 
             </div>
--- src/components/ui/FeatureList.tsx
+++ src/components/ui/FeatureList.tsx
@@ -3,11 +3,13 @@ import GradientTitle from './GradientTitle';
 import CheckListItem from './CheckListItem';
 import { IconType } from 'react-icons';
 
-const FeatureList: React.FC<{title: string, features: string[], icon?: IconType}> = ({title, features, icon: Icon}) => {
+const FeatureList: React.FC<{ title: string, features: string[], icon?: IconType }> = ({ title, features, icon: Icon }) => {
     return (
         <div className='flex flex-col gap-3 '>
-            {Icon && <Icon />}
-            <GradientTitle>{title}</GradientTitle>
+            <div className='flex justify-start items-center gap-3'>
+                {Icon && <Icon className='text-3xl' />}
+                <GradientTitle>{title}</GradientTitle>
+            </div>
             <div className='flex flex-col gap-3'>
                 {
                     features.map((feature, index) => <CheckListItem key={index} title={feature} />)
